f60834f8b4f711a4bf88885db1cd14907c190fe5
Md Rahi <fhrfhr102030@gmail.com>
2023-05-05 19:56:57 +0600

    load specefic id data

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const core_concepts = require('./Data/CoreConcepts.json'); 
+
 app.get('/', (req, res) => {
     res.send("React Station Server Running");
+});
+
+app.get('/core-concepts', (req, res) => {
+    res.send(core_concepts);
+});
+
+app.get('/core-concepts/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedConcept = core_concepts.find(concept => concept.id === id);
+    res.send(selectedConcept);
 });
 
 app.listen(port, () =>{
