ff80bcda99b5ac12911214f94c0a15fbe7686f05
Md Rahi <fhrfhr102030@gmail.com>
2023-05-07 19:09:02 +0600

    create feedbacks using post method

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 const core_concepts = require('./Data/CoreConcepts.json'); 
+const users_messages = require('./Data/UsersMessage.json'); 
 
 app.get('/', (req, res) => {
     res.send("React Station Server Running");
@@ -19,6 +21,18 @@ app.get('/core-concepts/:id', (req, res) => {
     const id = req.params.id;
     const selectedConcept = core_concepts.find(concept => concept.id === id);
     res.send(selectedConcept);
+});
+
+app.post('/feedbacks', (req, res) => {
+    const feedbacks = req.body;
+    feedbacks.id = users_messages.length + 1;
+    users_messages.push(feedbacks);
+    res.send(feedbacks);
+    console.log(feedbacks);
+})
+
+app.get('/feedbacks', (req, res) => {
+    res.send(users_messages);
 });
 
 app.listen(port, () =>{
